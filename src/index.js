f7e8238612d80a71c58e8236a156ebce6ecf163c
patoshim3 <patimaactposhta@gmail.com>
2026-02-28 22:33:37 +0500

    changes by patoshim

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ app.use('/api/progress', progressRoutes)
 app.use('/api/tasks', taskPublicRoutes)
 app.use('/api/tasks', taskAudioRoutes)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
